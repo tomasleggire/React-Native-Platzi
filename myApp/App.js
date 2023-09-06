72c36191e7c556e9e96bd6db5f5559168a1800fe
Tomás <tomasleggire@hotmail.com>
2023-09-06 20:46:47 -0300

    Props

--- myApp/App.js
+++ myApp/App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginForm from "./src/components/LoginForm";
+import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
-      <LoginForm />
+      <Text>Curso React Native</Text>
+      <Saludar name="Tomas" />
+      <Saludar name="Carlos" />
+      <Saludar name="Juan" />
     </View>
   );
 }
---
+++ myApp/src/components/Saludar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Saludar(props) {
+  const { name } = props;
+  return <Text>Hola {name}</Text>;
+}
